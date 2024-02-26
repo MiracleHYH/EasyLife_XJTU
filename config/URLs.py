0b00d077641b47cc2b0b1e63bbc7b5d28185538e
Miracle <miracle20001110@gmail.com>
2024-02-26 16:51:51 +0800

    update url

--- config/URLs.py
+++ config/URLs.py
@@ -4,12 +4,16 @@
 webvpn_login_url = "https://webvpn.xjtu.edu.cn/login?oauth_login=true"
 webvpn_url = "https://webvpn.xjtu.edu.cn"
 
-# 交大研究生体美劳育管理平台
+# 交大研究生体美劳育管理平台 (手机版)
+# 登陆跳转
 tmlyglpt_login_url = "https://org.xjtu.edu.cn/openplatform/oauth/authorize?appId=1532&redirectUri=https://ipahw.xjtu.edu.cn/sso/callback&responseType=code&scope=user_info&state=1234"
+# 我的活动
 tmlyglpt_wdhd_url = "https://ipahw.xjtu.edu.cn/pages/index/wdhd"
+# 我的活动-运动签到
 tmlyglpt_ydqd_url = "https://ipahw.xjtu.edu.cn/pages/index/hdgl/hdgl_run?courseType=7&signType=1&activityAddress=&courseInfoId=1698877075970076673"
+# 我的活动-运动签退
 tmlyglpt_ydqt_url = "https://ipahw.xjtu.edu.cn/pages/index/hdgl/hdgl_run?courseType=7&signType=2&activityAddress=&courseInfoId=1698877075970076673"
-tmlyglpt_ydxx_api = "https://ipahw.xjtu.edu.cn/szjy-boot/api/v1/sportActa/findPage?pageNo=1&pageSize=10&sportType_EQ=2&createTime_ORDER=DESC&courseInfoId_EQ=1698877075970076673&no_EQ={username}"
+# 
 tmlyglpt_ydqd_api = "https://ipahw.xjtu.edu.cn/szjy-boot/api/v1/sportActa/signRun"
 tmlyglpt_ydqt_api = "https://ipahw.xjtu.edu.cn/szjy-boot/api/v1/sportActa/signOutTrain"
 
