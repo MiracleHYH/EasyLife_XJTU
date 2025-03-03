238a1fbc87eb9087be3c82d83759ba5fc0b7677f
Yuheng He <miracle20001110@gmail.com>
2025-03-03 15:19:44 +0800

    fix: switch to mobile

--- utils/login.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from config import URLs
-
-
-def login(driver, wait, username, password):
-    pass
