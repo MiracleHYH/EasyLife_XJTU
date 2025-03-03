e8a7c2954fa2fc8c75ec1c2796594d75d7c569a4
Yuheng He <miracle20001110@gmail.com>
2025-03-03 15:00:10 +0800

    fix mobile

--- AutoSignin.py
+++ AutoSignin.py
@@ -15,7 +15,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import logging
 
-logger = logging.getLogger("AutoSports")
+logger = logging.getLogger("AutoSignin")
 console = logging.StreamHandler()
 formatter = logging.Formatter('[%(levelname)s] %(message)s')
 console.setFormatter(formatter)
@@ -25,50 +25,60 @@ logger.setLevel(logging.INFO)
 
 def work(username, password):
     options = Options()
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-infobars")
-    # options.add_experimental_option("mobileEmulation", {"deviceName": "iPhone 14 Pro Max"})
-    options.add_experimental_option("prefs", {
-        "profile.default_content_setting_values.geolocation": 1  # 允许地理位置 (Allow geolocation)
-    })
+    # options.add_argument("--incognito")
+    options.add_experimental_option("prefs", {"profile.default_content_setting_values.geolocation": 1})
+    options.add_experimental_option("mobileEmulation", {"deviceName": "iPhone 14 Pro Max"})
     driver = webdriver.Chrome(options=options)
-    wait = WebDriverWait(driver, 15)
+    wait = WebDriverWait(driver, 10)
 
+    # logger.info(f"账号{username}设置地理位置")
+    driver.execute_cdp_cmd("Emulation.setGeolocationOverride", {
+        "latitude": 34.24764385304397,  # 纬度 (Latitude)
+        "longitude": 108.98003946842356,  # 经度 (Longitude)
+        "accuracy": 100  # 精度 (Accuracy)
+    })
     driver.get(URLs.iair_signin_home_url)
-    username_input = wait.until(EC.presence_of_element_located((By.NAME, 'username')))
-    password_input = wait.until(EC.presence_of_element_located((By.NAME, 'pwd')))
+
+    username_input = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@class='isMObil']//input[@class='username']")))
+    password_input = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='isMObil']//input[@class='pwd']")))
 
     username_input.send_keys(username)
     password_input.send_keys(password)
 
     password_input.send_keys(Keys.RETURN)
 
-    # signin_tab = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@role='treeitem' and .//div[contains(@class, 'content-title') and text()='打卡']]")))
-    # signin_tab.click()
-    sleep(5)
-    driver.get(URLs.iair_signin_form_url)
-    driver.execute_cdp_cmd("Emulation.setGeolocationOverride", {
-        "latitude": 34.24764385304397,   # 纬度 (Latitude)
-        "longitude": 108.98003946842356, # 经度 (Longitude)
-        "accuracy": 100        # 精度 (Accuracy)
-    })
-    location_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'location-btn')))
+    location_button = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//div[contains(@class,'ry-form__location-compact')]//button")))
     location_button.click()
+    sleep(10)
+
+    # logger.info(f"账号{username}点击获取地理位置")
     confirm_button = wait.until(EC.element_to_be_clickable(
         (By.XPATH, "//div[contains(@class, 'handle-btn') and normalize-space(text())='确定']")
     ))
-    sleep(3)
     confirm_button.click()
+    # logger.info(f"账号{username}确认地理位置")
 
     sleep(3)
     submit_button = wait.until(EC.element_to_be_clickable(
-        (By.XPATH, "//div[contains(@class, 'ry-form__fill-show-btn') and contains(normalize-space(.), '提交')]")
+        (By.XPATH, "//div[contains(@class, 'only-btn') and normalize-space(text())='提交']")
     ))
+    # logger.info(f"账号{username}点击提交按钮")
     submit_button.click()
+    # driver.execute_script("arguments[0].click();", submit_button)
+    # time.sleep(5)
+    # logger.info(f"账号{username}提交成功,等待成功确认")
+    wait.until(EC.visibility_of_element_located(
+        (By.XPATH, "//div[@class='van-toast__text' and normalize-space(text())='提交成功']")
+    ))
+    driver.quit()
 
 
 if __name__ == '__main__':
--- Task_AutoSigninIAIR.py
+++ Task_AutoSigninIAIR.py
@@ -3,7 +3,8 @@
 # Desc: XJTU_IAIR研究生自动签到
 """
 new Env('XJTU_IAIR研究生自动签到');
-# cron: 50 7,13 * * *
+# cron: 50 7 * * *
+# cron: 50 13 * * *
 """
 
 import os
@@ -31,19 +32,17 @@ logger.setLevel(logging.INFO)
 
 def work(username, password):
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-infobars")
-    # options.add_experimental_option("mobileEmulation", {"deviceName": "iPhone 14 Pro Max"})
-    options.add_experimental_option("prefs", {
-        "profile.default_content_setting_values.geolocation": 1  # 允许地理位置 (Allow geolocation)
-    })
+    # options.add_argument("--incognito")
+    options.add_experimental_option("prefs", {"profile.default_content_setting_values.geolocation": 1})
+    options.add_experimental_option("mobileEmulation", {"deviceName": "iPhone 14 Pro Max"})
     driver = webdriver.Chrome(options=options)
-    wait = WebDriverWait(driver, 15)
+    wait = WebDriverWait(driver, 10)
 
     # logger.info(f"账号{username}设置地理位置")
     driver.execute_cdp_cmd("Emulation.setGeolocationOverride", {
@@ -51,61 +50,40 @@ def work(username, password):
         "longitude": 108.98003946842356,  # 经度 (Longitude)
         "accuracy": 100  # 精度 (Accuracy)
     })
-
     driver.get(URLs.iair_signin_home_url)
-    username_input = wait.until(EC.presence_of_element_located((By.NAME, 'username')))
-    password_input = wait.until(EC.presence_of_element_located((By.NAME, 'pwd')))
+
+    username_input = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@class='isMObil']//input[@class='username']")))
+    password_input = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='isMObil']//input[@class='pwd']")))
 
     username_input.send_keys(username)
     password_input.send_keys(password)
 
     password_input.send_keys(Keys.RETURN)
 
-    # logger.info(f"账号{username}登录成功")
-
-    # signin_tab = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@role='treeitem' and .//div[contains(@class, 'content-title') and text()='打卡']]")))
-    # signin_tab.click()
-    sleep(5)
-
-
-    driver.get(URLs.iair_signin_form_url)
-    # sleep(10)
-    # logger.info(f"账号{username}进入打卡页面")
-    location_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'location-btn')))
+    location_button = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//div[contains(@class,'ry-form__location-compact')]//button")))
     location_button.click()
+    sleep(10)
+
     # logger.info(f"账号{username}点击获取地理位置")
-    sleep(3)
     confirm_button = wait.until(EC.element_to_be_clickable(
         (By.XPATH, "//div[contains(@class, 'handle-btn') and normalize-space(text())='确定']")
     ))
-    location_dialog = wait.until(EC.presence_of_element_located(
-        (By.CLASS_NAME, 'location-dialog')
-    ))
-    logger.info(f"账号{username} location dialog: {location_dialog.text}")
     confirm_button.click()
     # logger.info(f"账号{username}确认地理位置")
 
     sleep(3)
     submit_button = wait.until(EC.element_to_be_clickable(
-        (By.XPATH, "//div[contains(@class, 'ry-form__fill-show-btn') and contains(normalize-space(.), '提交')]")
+        (By.XPATH, "//div[contains(@class, 'only-btn') and normalize-space(text())='提交']")
     ))
-    location_info = wait.until(EC.presence_of_element_located(
-        (By.CLASS_NAME, 'location-item')
-    ))
-    logger.info(f"账号{username}的地理位置信息: {location_info.text}")
     # logger.info(f"账号{username}点击提交按钮")
-    driver.execute_script("arguments[0].click();", submit_button)
-    # time.sleep(5)
+    submit_button.click()
     # logger.info(f"账号{username}提交成功,等待成功确认")
     wait.until(EC.visibility_of_element_located(
-        (By.XPATH, "//div[@role='alert' and contains(normalize-space(.), '提交成功')]")
+        (By.XPATH, "//div[@class='van-toast__text' and normalize-space(text())='提交成功']")
     ))
-
-    # time.sleep(30000)
-    # driver.execute_script("arguments[0].scrollIntoView();", submit_button)
-    # sleep(3)
-    # submit_button.click()
-    # time.sleep(3)
+    driver.quit()
 
 
 if __name__ == '__main__':
@@ -122,7 +100,7 @@ if __name__ == '__main__':
     logger.info(f"共有{len(auths)}个账号")
 
     # 在9~600秒内随机等待
-    # sleep(int(9 + 591 * random()))
+    sleep(int(9 + 591 * random()))
 
     for auth in auths:
         max_trial = 3
